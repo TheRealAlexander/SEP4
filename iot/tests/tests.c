5c1ea0e860de2029eed8a26039aa82d7153cb98b
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2024-04-05 09:09:21 +0200

    Test HTTP parse response

--- iot/tests/tests.c
+++ iot/tests/tests.c
@@ -164,6 +164,55 @@ static void test_http_generate_request_no_overflow() {
 }
 
 ////////////////////////////////////////////////////////////////
+//- Parse http response tests
+
+static void test_http_parse_response() {
+    // NOTE(rune): Example response from an ASP.NET web API
+    string res = string(
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Content-Type: text/plain; charset=utf-8\r\n"
+        "Date: Thu, 04 Apr 2024 11:25:22 GMT\r\n"
+        "Server: Kestrel\r\n"
+        "\r\n"
+        "So what's it all about?"
+    );
+
+    int status_code   = { 0 };
+    string name       = { 0 };
+    string value      = { 0 };
+    bool more_headers = { 0 };
+
+    http_read_head_begin(&res, &status_code);
+    assert_int(status_code, 200);
+
+    more_headers = http_read_head_string(&res, &name, &value);
+    assert_string(name, string("Connection"));
+    assert_string(value, string("close"));
+    assert_bool(more_headers, true);
+
+    more_headers = http_read_head_string(&res, &name, &value);
+    assert_string(name, string("Content-Type"));
+    assert_string(value, string("text/plain; charset=utf-8"));
+    assert_bool(more_headers, true);
+
+    more_headers = http_read_head_string(&res, &name, &value);
+    assert_string(name, string("Date"));
+    assert_string(value, string("Thu, 04 Apr 2024 11:25:22 GMT"));
+    assert_bool(more_headers, true);
+
+    more_headers = http_read_head_string(&res, &name, &value);
+    assert_string(name, string("Server"));
+    assert_string(value, string("Kestrel"));
+    assert_bool(more_headers, false);
+
+    http_read_head_end(&res);
+
+    // NOTE(rune): Body should be the only thing remaining.
+    assert_string(res, string("So what's it all about?"));
+}
+
+////////////////////////////////////////////////////////////////
 //- Test runner
 
 #define run_test(func) (func(), printf(#func " (OK)\n"))
@@ -172,6 +221,7 @@ int main() {
     run_test(test_http_generate_get_request);
     run_test(test_http_generate_post_request);
     run_test(test_http_generate_request_no_overflow);
+    run_test(test_http_parse_response);
     printf("All tests suceeded\n");
     return 0;
 }
