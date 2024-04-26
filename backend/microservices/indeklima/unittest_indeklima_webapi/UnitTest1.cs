fac112a141b642db7f3bd03a5ce0fa5ac5a8336a
Olsen7351 <olsen7351@gmail.com>
2024-04-26 11:32:14 +0200

    Test: Added empty test project to indeklima api

---
+++ backend/microservices/indeklima/unittest_indeklima_webapi/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace unittest_indeklima_webapi;
+
+public class UnitTest1
+{
+    
+
+    //Setup the test environment
+    [SetUp]
+    public void SetUp() {
+        
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
