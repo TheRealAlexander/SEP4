2b82ad2768c9525c6c76857bade2d64e7857e84e
Simone <simonestormbk@gmail.com>
2024-04-26 10:29:34 +0200

    Fix import statement in App.js

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -16,7 +16,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+
 
 const pages = ['Home', 'Controls', 'Data'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
