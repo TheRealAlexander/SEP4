296159f4a7c3524bc9a0a721c28da895191456eb
Mathias Milling Hansen <112928995+MathiasMillingHansen@users.noreply.github.com>
2024-05-05 11:34:19 +0200

    Added enable user to create tournament function

--- frontend/app/src/Services/UserService.js
+++ frontend/app/src/Services/UserService.js
@@ -12,12 +12,25 @@ class UserService {
   }
 
   // Update user
-  static async updateUser(userToMakeAdmin) {
+  static async makeUserAdmin(userToMakeAdmin) {
     try {
       const user = JSON.parse(localStorage.getItem('user'));
       const response = await axios.put(`http://localhost:8000/users/admins/${user.id}`, {
         ...user,
         ...userToMakeAdmin
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async enableUserToCreateTournaments(userToChange) {
+    try {
+      const user = JSON.parse(localStorage.getItem('user'));
+      const response = await axios.put(`http://localhost:8000/users/tournamentUsers/${user.id}`, {
+        ...user,
+        ...userToChange
       });
       return response.data;
     } catch (error) {
