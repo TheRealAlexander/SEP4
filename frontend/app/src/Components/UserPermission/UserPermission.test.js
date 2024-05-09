c09d09578ecf273a3d9d8133343a5a7622c9da66
SantaDrugen <emil0510@hotmail.com>
2024-05-09 20:55:52 +0200

    Wrapper-function added to export UserPermission page (used to fetch data on load when ready).

--- frontend/app/src/Components/UserPermission/UserPermission.test.js
+++ frontend/app/src/Components/UserPermission/UserPermission.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import UserList from './UserPermission';
+import { UserList } from './UserPermission';
 
 describe('UserList Component', () => {
   const adminUsers = [{ id: 1, name: 'Admin1' }];
