4756791e014100e2714a9eb9f7bff9f02afd4745
SimoneStorm <114076179+SimoneStorm@users.noreply.github.com>
2024-05-10 10:05:05 +0200

    Frontend/feature/room preferences issues 55 og 49 (#64)
    # Pull Request (PR) Template
    
    ## Description
    Tilføjet componenter til controls siden for hal oversigt
    klimapræferencer og baneoversigt
    
    ## Type of Change
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] Documentation Update
    - [ ] Other (please describe)
    
    ## Checklist
    Before submitting your PR, please ensure the following:
    - [x] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have made corresponding changes to the documentation (if
    appropriate).
    - [x] My changes generate no new warnings.
    - [ ] I have added tests that prove my fix is effective or that my
    feature works.
    - [ ] All new and existing tests passes.
    
    ## Screenshots (if appropriate)
    
    ## Additional Notes
    Include any additional information that you believe is important for the
    reviewers to know.
    
    ---------
    
    Co-authored-by: Rasmus Bigum Kallehave Nielsen <129666566+Rasmus-BK-Nielsen@users.noreply.github.com>

--- frontend/app/src/Components/Sidebar/Sidebar.js
+++ frontend/app/src/Components/Sidebar/Sidebar.js
@@ -1,65 +1,120 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import {ResponsiveAppBar} from '../../App.js';
+import React, { useState } from 'react';
+import { Box, Card, CardActionArea, CardContent, Typography, Grid, Container, Button } from '@mui/material';
+import TemperatureHumidityControl from '../HumidityTemperaturePreferences/TemperatureHumidity';
+import LightControl from '../LightControl/LightControl';
+import CurrentConditions from '../CurrentConditons/CurrentConditions';
 
-const drawerWidth = 240;
+function HomePage() {
+    const [selectedHal, setSelectedHal] = useState(null);
+    const [controlStates, setControlStates] = useState({
+        'Hal 1': { temperature: 22, humidity: 50, lights: Array(4).fill(50) }, 
+        'Hal 2': { temperature: 22, humidity: 50, lights: Array(5).fill(50) },
+        'Hal 3': { temperature: 22, humidity: 50, lights: Array(7).fill(50) }, 
+    });
 
-export default function Layout() {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-            marginTop: '64px' 
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        
-        <Box sx={{ overflow: 'auto' }}>
-          <List>
-            {['Hal 1', 'Hal 2', 'Hal 3'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          marginTop: '64px'  // Adjust this value based on the actual height of your AppBar
-        }}
-      >
-        <Typography paragraph>
-          Content
-        </Typography>
-      </Box>
-    </Box>
-  );
+    const [tempControlStates, setTempControlStates] = useState({ ...controlStates });
+
+    const cardStyles = (isActive) => ({
+        width: 300,
+        height: 150,
+        margin: 6,
+        backgroundColor: isActive ? 'lightblue' : 'white',
+        transition: '0.3s',
+        '&:hover': {
+            backgroundColor: 'lightblue',
+            boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)'
+        }
+    });
+
+    const handleSave = () => {
+      setControlStates(tempControlStates);
+  };
+
+  const handleCancel = () => {
+      setTempControlStates({ ...controlStates });
+  };
+
+  const updateTemperature = (hal, newValue) => {
+      setTempControlStates(prev => ({
+          ...prev,
+          [hal]: { ...prev[hal], temperature: newValue }
+      }));
+  };
+
+  const updateHumidity = (hal, newValue) => {
+      setTempControlStates(prev => ({
+          ...prev,
+          [hal]: { ...prev[hal], humidity: newValue }
+      }));
+  };
+
+  const updateLightIntensity = (hal, laneId, newValue) => {
+      setTempControlStates(prev => ({
+          ...prev,
+          [hal]: {
+              ...prev[hal], 
+              lights: prev[hal].lights.map((intensity, index) => index === laneId - 1 ? newValue : intensity)
+          }
+      }));
+    };
+
+    return (
+      <Container sx={{ paddingTop: '75px', minHeight: '100vh', overflowY: 'auto', paddingLeft: '64px',  marginLeft: '200px' }}>
+          <Box sx={{ marginLeft: '32px' }}> 
+              <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginBottom: 4 }}>
+                  {['Hal 1', 'Hal 2', 'Hal 3'].map((hal, index) => (
+                      <Card sx={{ width: 300, height: 150, margin: 6, backgroundColor: selectedHal === hal ? 'lightblue' : 'white', transition: '0.3s', '&:hover': { backgroundColor: 'lightblue', boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)' } }} key={index}>
+                          <CardActionArea 
+                              onClick={() => setSelectedHal(hal)}
+                              sx={{
+                                  height: '100%',
+                                  width: '100%',
+                                  display: 'flex',
+                                  flexDirection: 'column',
+                                  justifyContent: 'center'
+                              }}
+                          >
+                              <CardContent>
+                                  <Typography variant="h5" sx={{ textAlign: 'center' }}>{hal}</Typography>
+                              </CardContent>
+                          </CardActionArea>
+                      </Card>
+                  ))}
+              </Box>
+              {selectedHal && (
+                  <Grid container spacing={2} justifyContent="flex-end">
+                      <Grid item xs={4} sx={{ overflowY: 'auto', maxHeight: 500 }}>
+                          <Box>
+                              {tempControlStates[selectedHal].lights.map((intensity, index) => (
+                                  <LightControl
+                                      key={index}
+                                      laneId={index + 1}
+                                      initialIntensity={intensity}
+                                      onIntensityChange={(newIntensity) => updateLightIntensity(selectedHal, index + 1, newIntensity)}
+                                  />
+                              ))}
+                          </Box>
+                      </Grid>
+                      <Grid item xs={4}>
+                          <TemperatureHumidityControl 
+                              initialTemperature={tempControlStates[selectedHal].temperature}
+                              initialHumidity={tempControlStates[selectedHal].humidity}
+                              onTemperatureChange={(newValue) => updateTemperature(selectedHal, newValue)}
+                              onHumidityChange={(newValue) => updateHumidity(selectedHal, newValue)}
+                          />
+                      </Grid>
+                      <Grid item xs={4}>
+                          <CurrentConditions 
+                              temperature={tempControlStates[selectedHal].temperature}
+                              humidity={tempControlStates[selectedHal].humidity}
+                          />
+                          <Button onClick={handleSave} variant="contained" color="primary" sx={{ margin: 1 }}>Save Changes</Button>
+                          <Button onClick={handleCancel} variant="outlined" color="secondary" sx={{ margin: 1 }}>Cancel</Button>
+                      </Grid>
+                  </Grid>
+              )}
+          </Box>
+      </Container>
+    );
 }
+export default HomePage;
