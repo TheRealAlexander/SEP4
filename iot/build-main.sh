0b424792a4082ccf930c29c9fbc85dc555cd9090
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2024-05-14 12:02:41 +0200

    Fix build-main.sh

--- iot/build-main.sh
+++ iot/build-main.sh
@@ -1,3 +1,3 @@
 mkdir -p build
-avr-gcc -mmcu=atmega2560 -o build/main.o src/main.c && avr-objcopy -j .text -j .data -O ihex build/main.o build/main.hex
+avr-gcc -mmcu=atmega2560 -o build/main.o src/main.c -DNOT_PLATFORMIO -DF_CPU=16000000 -O2 && avr-objcopy -j .text -j .data -O ihex build/main.o build/main.hex
 
