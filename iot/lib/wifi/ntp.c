3bbaac11c0d790ecc019552ff06f5195c000183f
TheRealAlexander <114055261+TheRealAlexander@users.noreply.github.com>
2024-05-16 23:43:27 +0200

    Replaced magic numbers with define directives

--- iot/lib/wifi/ntp.c
+++ iot/lib/wifi/ntp.c
@@ -1,13 +1,17 @@
 #include "ntp.h"
 
+#define LI_NO_LEAP_SECOND 0
+#define VN_NTP_VERSION 3
+#define MODE_CLIENT 3
+#define MODE_SERVER 4
+#define STRATUM_MIN 1
+#define STRATUM_MAX 15
+
 void construct_ntp_request(ntp_request_packet* packet) {
     memset(packet, 0, sizeof(ntp_request_packet));  // Initialize all fields to 0
 
     // Set the li, vn, and mode fields.
-    // li:   We set it to 0 (no leap second adjustment).
-    // vn:   We set it to 3 (version number of the NTP protocol).
-    // mode: We set it to 3 (client mode).
-    packet->li_vn_mode = (0 << 6) | (3 << 3) | 3;
+    packet->li_vn_mode = (LI_NO_LEAP_SECOND << 6) | (VN_NTP_VERSION << 3) | MODE_CLIENT;
 }
 
 void decode_ntp_response(const uint8_t* buffer, ntp_response_packet* packet) {
@@ -33,18 +37,18 @@ bool is_ntp_response_packet(const uint8_t* buffer, size_t size) {
 
     // Check the version number (VN) field. It should be 3 or 4.
     uint8_t vn = (packet->li_vn_mode >> 3) & 0x07;
-    if (vn != 3 && vn != 4) {
+    if (vn != VN_NTP_VERSION && vn != VN_NTP_VERSION + 1) {
         return false;
     }
 
     // Check the mode field. It should be 4, if from a server.
     uint8_t mode = packet->li_vn_mode & 0x07;
-    if (mode != 4) {
+    if (mode != MODE_SERVER) {
         return false;
     }
 
     // Check the stratum field. It should be between 1 and 15 for a valid server.
-    if (packet->stratum == 0 || packet->stratum > 15) {
+    if (packet->stratum < STRATUM_MIN || packet->stratum > STRATUM_MAX) {
         return false;
     }
 
