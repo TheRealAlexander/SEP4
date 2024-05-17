7386921de02d5e8041f9918da0740b69c50a55a6
SantaDrugen <emil0510@hotmail.com>
2024-05-17 18:29:26 +0200

    Fixed test to reflect changes to code

--- frontend/app/src/Profile/Registration/Registration.test.js
+++ frontend/app/src/Profile/Registration/Registration.test.js
@@ -2,39 +2,43 @@ import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import { useNavigate } from 'react-router-dom';
 import AuthService from '../../Services/AuthService';
-import Registration from './Registration';
+import RegistrationPage from './Registration';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 jest.mock('../../Services/AuthService', () => ({
-    register: jest.fn().mockReturnValue(Promise.resolve()),
-  }));
+  register: jest.fn().mockReturnValue(Promise.resolve()),
+}));
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: jest.fn(),
 }));
 
-describe('Registration', () => {
+describe('RegistrationPage', () => {
   it('calls AuthService.register with the correct arguments when the form is submitted', async () => {
     const mockNavigate = jest.fn();
     useNavigate.mockReturnValue(mockNavigate);
 
-    const { getByLabelText, getByRole } = render(
-        <Router>
-          <Registration />
-        </Router>
-      );
+    render(
+      <Router>
+        <RegistrationPage />
+      </Router>
+    );
 
     const usernameInput = screen.getByLabelText(/username/i);
     const passwordInput = screen.getByLabelText(/password/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const ageInput = screen.getByLabelText(/age/i);
     const submitButton = screen.getByRole('button', { name: /register/i });
 
     fireEvent.change(usernameInput, { target: { value: 'testuser' } });
     fireEvent.change(passwordInput, { target: { value: 'testpass' } });
+    fireEvent.change(emailInput, { target: { value: 'testuser@example.com' } });
+    fireEvent.change(ageInput, { target: { value: '30' } });
     fireEvent.click(submitButton);
 
     await waitFor(() => {
-      expect(AuthService.register).toHaveBeenCalledWith('testuser', 'testpass');
+      expect(AuthService.register).toHaveBeenCalledWith('testuser', 'testpass', 'testuser@example.com', '30');
     });
   });
 });
