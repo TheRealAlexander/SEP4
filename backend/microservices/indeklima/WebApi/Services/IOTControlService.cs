834bfb5715d4e0c4bbe3abee6e7ec13135506c6d
Emil Brügge <equistgaard@yahoo.dk>
2024-05-17 09:45:48 +0200

    Threshold removed

--- backend/microservices/indeklima/WebApi/Services/IOTControlService.cs
+++ backend/microservices/indeklima/WebApi/Services/IOTControlService.cs
@@ -2,15 +2,12 @@
 
 public class IOTControlService : IIOTControlService
 {
-    private const double TemperatureThreshold = 1.0; // Threshold
-    private const double HumidityThreshold = 3.0; // Threshold
-    private const double Co2Threshold = 30.0; // Threshold (in ppm)
-
+    
     public bool ShouldWindowOpen(double currentTemperature, double desiredTemperature, double currentHumidity, double desiredHumidity, double currentCo2, double desiredCo2)
     {
-        bool shouldOpenForTemperature = currentTemperature > (desiredTemperature + TemperatureThreshold);
-        bool shouldOpenForHumidity = currentHumidity > (desiredHumidity + HumidityThreshold);
-        bool shouldOpenForCo2 = currentCo2 > (desiredCo2 + Co2Threshold);
+        bool shouldOpenForTemperature = currentTemperature > desiredTemperature;
+        bool shouldOpenForHumidity = currentHumidity > desiredHumidity;
+        bool shouldOpenForCo2 = currentCo2 > desiredCo2;
 
         // Open the window if any of the conditions are met
         return shouldOpenForTemperature || shouldOpenForHumidity || shouldOpenForCo2;
