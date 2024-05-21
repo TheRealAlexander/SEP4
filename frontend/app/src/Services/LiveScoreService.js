5223365348e18fc34d205a0ed58fd4e4b3ef989a
Mathias Milling Hansen <331199@via.dk>
2024-05-21 10:25:35 +0200

    Api fix for livescoreservice.js

--- frontend/app/src/Services/LiveScoreService.js
+++ frontend/app/src/Services/LiveScoreService.js
@@ -1,25 +1,27 @@
-﻿import axios from 'axios';
+﻿import axios from "axios";
+
+const API_BASE_URL = "http://broker:5202/Broker/";
 
 class LiveScoreService {
-    static async getLiveScore(id) {
-        try {
-            const response = await axios.get(`http://localhost:5202/Broker/LiveScore/${id}`);
-            return response.data;
-        } catch (error) {
-            console.error('Error fetching live score:', error);
-            throw error;
-        }
+  static async getLiveScore(id) {
+    try {
+      const response = await axios.get(API_BASE_URL + `LiveScore/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching live score:", error);
+      throw error;
     }
+  }
 
-    static async getAllMatches() {
-        try {
-            const response = await axios.get(`http://localhost:5202/Broker/LiveScore`);
-            return response.data;
-        } catch (error) {
-            console.error('Error fetching all matches:', error);
-            throw error;
-        }
+  static async getAllMatches() {
+    try {
+      const response = await axios.get(API_BASE_URL + `LiveScore`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching all matches:", error);
+      throw error;
     }
+  }
 }
 
 export default LiveScoreService;
