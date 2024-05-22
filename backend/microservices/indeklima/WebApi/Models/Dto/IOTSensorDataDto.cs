c5d766a8811336ad3e847bcde76539ab7c4ab4fa
Emil Brügge <equistgaard@yahoo.dk>
2024-05-22 13:48:08 +0200

    Reworked timestamps, reworked tests, removed unnecessary imports.

--- backend/microservices/indeklima/WebApi/Models/Dto/IOTSensorDataDto.cs
+++ backend/microservices/indeklima/WebApi/Models/Dto/IOTSensorDataDto.cs
@@ -6,5 +6,8 @@
         public double Temperature { get; set; }
         public double Humidity { get; set; }
         public double CO2 { get; set; }
+        public long Temperature_ts { get; set; } // Name needs to match the json object IoT sends
+        public long Humidity_ts { get; set; } // Name needs to match the json object IoT sends
+        public long Co2_ts { get; set; } // Name needs to match the json object IoT sends
     }
 }
