d4b7b0b6941102c760ce37a795e393d7cf4ff0bb
Rasmus Bigum Kallehave Nielsen <129666566+Rasmus-BK-Nielsen@users.noreply.github.com>
2024-05-23 10:52:39 +0200

    Combined functions in the climateService to better control amount of requests for data. Functions in question are: setPreferredValue and useSensorData. Also added a function to control windowState.

--- frontend/app/src/Components/CO2/CO2Wrapper.js
+++ frontend/app/src/Components/CO2/CO2Wrapper.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import CO2Component from './CO2Comp';
-import { useHumidityData } from '../../Services/ClimateService';
+import { useSensorData } from '../../Services/ClimateService';
 
 export default function HumidityDataWrapper() {
-     // TEMP DATA:
-     let CO2Data = {
-          co2_concentration: 1000
-     };
-
-     // CHANGE TO USE CORRECT DATASERVICE!!!!!!!!!
-    let { CO2Data_T, isLoading } = useHumidityData();
+    let { CO2Data, isLoading } = useSensorData();
 
     return <CO2Component humidityData={CO2Data} isLoading={isLoading} />;
 }
