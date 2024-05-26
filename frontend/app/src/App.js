03ed5044e67c7adf688cd01078990170e570d9c4
Mathias Milling Hansen <112928995+MathiasMillingHansen@users.noreply.github.com>
2024-05-26 10:49:34 +0200

    logo href changed to "/"

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -150,7 +150,7 @@ function ResponsiveAppBar() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
