0a08f900efca6da89aea2d92010880b3669748a5
Rune Lauridsen <57326389+runelauridsen@users.noreply.github.com>
2024-05-27 11:42:27 +0200

    Simplere config.h (#142)
    # Pull Request (PR) Template
    
    ## Description
    Gjort config af WiFi password, server ip, osv. simplere. 
    
    Før blev de læst fra enviroment variabler, som brugeren selv skulle
    sætte, og som build scriptet gav videre til compileren som makroer. I
    PlatformIO skulle man tilføje markroerne til `platformio.ini`.
    
    Nu skal man skrive i `iot\lib\config.h` i stedet.
    `iot\lib\config.h` bliver oprettet automatisk baseret på
    `iot\lib\config_default.h`, første gang man compiler med PlatformIO
    eller kører iot scriptet.
    
    Nu virker config ens om man bruger PlatformIO eller ej.
    
    `iot\lib\config.h` er i `.gitignore`, så man kan have sine egne
    personlige indstillinger.
    
    ## Type of Change
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] Documentation Update
    - [x] Other (please describe)
    
    ## Checklist
    Before submitting your PR, please ensure the following:
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have made corresponding changes to the documentation (if
    appropriate).
    - [ ] My changes generate no new warnings.
    - [ ] I have added tests that prove my fix is effective or that my
    feature works.
    - [ ] All new and existing tests passes.
    
    ## Screenshots (if appropriate)
    
    ## Additional Notes
    Include any additional information that you believe is important for the
    reviewers to know.

--- iot/iot.sh
+++ iot/iot.sh
@@ -3,8 +3,11 @@ clear
 set -e
 mkdir -p build
 
-# ////////////////////////////////////////////////////////////////
-# // Unpack arguments
+# Create config based on config_default.h
+if [ ! -e "lib/config.h" ]; then cp lib/config_default.h lib/config.h; fi
+
+# --------------------------------------------
+# Unpack arguments
 
 build=0
 upload=0
@@ -20,61 +23,31 @@ for arg in "$@"; do
     [ "$arg" == "test-local" ] && test_local=1
 done
 
-# ////////////////////////////////////////////////////////////////
-# // Set configuration variables
+# --------------------------------------------
+# AVR build, upload and test
 
-if [ -z "$SERVER_IP"     ]; then SERVER_IP="172.20.10.3"; fi
-if [ -z "$SERVER_PORT"   ]; then SERVER_PORT=5200; fi
-if [ -z "$WIFI_SSID"     ]; then WIFI_SSID="Rune - iPhone"; fi
-if [ -z "$WIFI_PASSWORD" ]; then WIFI_PASSWORD=123456789; fi
-if [ -z "$HALL_ID"       ]; then HALL_ID=37; fi
-
-# ////////////////////////////////////////////////////////////////
-# // Determine file to compile and determine compiler flags
-
-avr_flags="$avr_flags -mmcu=atmega2560"
-avr_flags="$avr_flags -O2"
-avr_flags="$avr_flags -Wall -Wextra"
-avr_flags="$avr_flags -D F_CPU=16000000 -D BAUD=9600"
-
-avr_flags="$avr_flags -D SERVER_IP='\"$SERVER_IP\"'"
-avr_flags="$avr_flags -D SERVER_PORT=$SERVER_PORT"
-avr_flags="$avr_flags -D WIFI_SSID='\"$WIFI_SSID\"'"
-avr_flags="$avr_flags -D WIFI_PASSWORD='\"$WIFI_PASSWORD\"'"
-avr_flags="$avr_flags -D HALL_ID=$HALL_ID"
-
+avr_flags="-mmcu=atmega2560 -O2 -Wall -Wextra -D F_CPU=16000000 -D BAUD=9600"
 avr_file="src/main.c"
 if [ "$test" -eq 1 ]; then
     avr_file="test/tests.c"
     avr_flags="$avr_flags -D TESTING=1"
 fi
 
-# ////////////////////////////////////////////////////////////////
-# // Build
-
 if [ "$build" -eq 1 ]; then
     eval avr-gcc $avr_file $avr_flags -o build/main.o
     avr-objcopy -j .text -j .data -O ihex build/main.o build/main.hex
 fi
 
-
-
-# ////////////////////////////////////////////////////////////////
-# // Upload to Arduino
-
 if [ "$upload" -eq 1 ]; then
     echo "Upload is not implemented for Linux"
 fi
-
-# ////////////////////////////////////////////////////////////////
-# // Serial monitor
 
 if [ "$listen" -eq 1 ]; then
     echo "Serial monitor is not implemented for Linux"
 fi
 
-# ////////////////////////////////////////////////////////////////
-# // Run test locally
+# --------------------------------------------
+# Local build and test
 
 if [ "$test_local" -eq 1 ]; then
     cc test/tests.c -o build/tests -DTESTING=1 -DMOCK=1 && build/tests
